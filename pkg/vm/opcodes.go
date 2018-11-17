4cb9e58a2a613170a0608e9b1776054bd7bd3fd2
Lee Keitel <lee@keitel.xyz>
2018-11-17 13:14:54 -0600

    Export a few methods on VM

--- pkg/vm/opcodes.go
+++ pkg/vm/opcodes.go
@@ -8,133 +8,143 @@ import (
 
 func (vm *VM) loadFromMem(r uint8, x uint16) {
 	switch {
-	case isDoubleReg(r):
-		vm.writeDoubleReg(r, uint16(vm.readMem(x, 2)))
+	case IsDoubleReg(Register(r)):
+		vm.writeDoubleReg(Register(r), uint16(vm.ReadMem(x, 2)))
 	default:
-		vm.writeReg(r, uint8(vm.readMem(x, 1)))
+		vm.writeSingleReg(Register(r), uint8(vm.ReadMem(x, 1)))
 	}
 }
 
 func (vm *VM) loadIntoReg(r uint8, x uint16) {
 	switch {
-	case isDoubleReg(r):
-		vm.writeDoubleReg(r, x)
+	case IsDoubleReg(Register(r)):
+		vm.writeDoubleReg(Register(r), x)
 	default:
-		vm.writeReg(r, uint8(x))
+		vm.writeSingleReg(Register(r), uint8(x))
 	}
 }
 
 func (vm *VM) loadRegInMemoryAddr(d, s uint8) {
-	addr := vm.readAnyReg(s)
+	addr := vm.ReadReg(Register(s))
 	vm.loadFromMem(d, addr)
 }
 
 func (vm *VM) storeRegToMemory(r uint8, x uint16) {
 	switch {
-	case isDoubleReg(r):
-		vm.writeMem(x, 2, vm.readDoubleReg(r))
+	case IsDoubleReg(Register(r)):
+		vm.WriteMem(x, 2, vm.readDoubleReg(Register(r)))
 	default:
-		vm.writeMem(x, 1, uint16(vm.readReg(r)))
+		vm.WriteMem(x, 1, uint16(vm.readSingleReg(Register(r))))
 	}
 }
 
 func (vm *VM) storeRegToRegAddr(s, d uint8) {
-	addr := vm.readAnyReg(d)
+	addr := vm.ReadReg(Register(d))
 	vm.storeRegToMemory(s, addr)
 }
 
 func (vm *VM) xferRegisters(r, s uint8) {
-	vm.writeAnyReg(r, vm.readAnyReg(s))
+	vm.WriteReg(Register(r), vm.ReadReg(Register(s)))
 }
 
 func (vm *VM) addAddr(d uint8, s uint16) {
+	dr := Register(d)
 	switch {
-	case isDoubleReg(d):
-		vm.writeDoubleReg(d, vm.readAnyReg(d)+vm.readMem16(s))
+	case IsDoubleReg(dr):
+		vm.writeDoubleReg(dr, vm.ReadReg(dr)+vm.readMem16(s))
 	default:
-		vm.writeReg(d, uint8(vm.readAnyReg(d))+vm.readMem8(s))
+		vm.writeSingleReg(dr, uint8(vm.ReadReg(dr))+vm.readMem8(s))
 	}
 }
 
 func (vm *VM) addImm(d uint8, s uint16) {
-	vm.writeAnyReg(d, vm.readAnyReg(d)+s)
+	dr := Register(d)
+	vm.WriteReg(dr, vm.ReadReg(dr)+s)
 }
 
 func (vm *VM) addReg(d uint8, s uint8) {
-	vm.writeAnyReg(d, vm.readAnyReg(s)+vm.readAnyReg(d))
+	vm.WriteReg(Register(d), vm.ReadReg(Register(s))+vm.ReadReg(Register(d)))
 }
 
 func (vm *VM) orAddr(d uint8, s uint16) {
+	dr := Register(d)
 	switch {
-	case isDoubleReg(d):
-		vm.writeDoubleReg(d, vm.readAnyReg(d)|vm.readMem16(s))
+	case IsDoubleReg(dr):
+		vm.writeDoubleReg(dr, vm.ReadReg(dr)|vm.readMem16(s))
 	default:
-		vm.writeReg(d, uint8(vm.readAnyReg(d))|vm.readMem8(s))
+		vm.writeSingleReg(dr, uint8(vm.ReadReg(dr))|vm.readMem8(s))
 	}
 }
 
 func (vm *VM) orImm(d uint8, s uint16) {
-	vm.writeAnyReg(d, vm.readAnyReg(d)|s)
+	dr := Register(d)
+	vm.WriteReg(dr, vm.ReadReg(dr)|s)
 }
 
 func (vm *VM) orReg(d uint8, s uint8) {
-	vm.writeAnyReg(d, vm.readAnyReg(s)|vm.readAnyReg(d))
+	vm.WriteReg(Register(d), vm.ReadReg(Register(s))|vm.ReadReg(Register(d)))
 }
 
 func (vm *VM) andAddr(d uint8, s uint16) {
+	dr := Register(d)
 	switch {
-	case isDoubleReg(d):
-		vm.writeDoubleReg(d, vm.readAnyReg(d)&vm.readMem16(s))
+	case IsDoubleReg(dr):
+		vm.writeDoubleReg(dr, vm.ReadReg(dr)&vm.readMem16(s))
 	default:
-		vm.writeReg(d, uint8(vm.readAnyReg(d))&vm.readMem8(s))
+		vm.writeSingleReg(dr, uint8(vm.ReadReg(dr))&vm.readMem8(s))
 	}
 }
 
 func (vm *VM) andImm(d uint8, s uint16) {
-	vm.writeAnyReg(d, vm.readAnyReg(d)&s)
+	dr := Register(d)
+	vm.WriteReg(dr, vm.ReadReg(dr)&s)
 }
 
 func (vm *VM) andReg(d uint8, s uint8) {
-	vm.writeAnyReg(d, vm.readAnyReg(s)&vm.readAnyReg(d))
+	vm.WriteReg(Register(d), vm.ReadReg(Register(s))&vm.ReadReg(Register(d)))
 }
 
 func (vm *VM) xorAddr(d uint8, s uint16) {
+	dr := Register(d)
 	switch {
-	case isDoubleReg(d):
-		vm.writeDoubleReg(d, vm.readAnyReg(d)^vm.readMem16(s))
+	case IsDoubleReg(dr):
+		vm.writeDoubleReg(dr, vm.ReadReg(dr)^vm.readMem16(s))
 	default:
-		vm.writeReg(d, uint8(vm.readAnyReg(d))^vm.readMem8(s))
+		vm.writeSingleReg(dr, uint8(vm.ReadReg(dr))^vm.readMem8(s))
 	}
 }
 
 func (vm *VM) xorImm(d uint8, s uint16) {
-	vm.writeAnyReg(d, vm.readAnyReg(d)^s)
+	dr := Register(d)
+	vm.WriteReg(dr, vm.ReadReg(dr)^s)
 }
 
 func (vm *VM) xorReg(d uint8, s uint8) {
-	vm.writeAnyReg(d, vm.readAnyReg(s)^vm.readAnyReg(d))
+	vm.WriteReg(Register(d), vm.ReadReg(Register(s))^vm.ReadReg(Register(d)))
 }
 
 func (vm *VM) rotrRegister(r, x uint8) {
+	rr := Register(r)
 	switch {
-	case isDoubleReg(r):
-		vm.writeDoubleReg(r, bits.RotateLeft16(vm.readDoubleReg(r), int(-x)))
+	case IsDoubleReg(rr):
+		vm.writeDoubleReg(rr, bits.RotateLeft16(vm.readDoubleReg(rr), int(-x)))
 	default:
-		vm.writeReg(r, bits.RotateLeft8(vm.readReg(r), int(-x)))
+		vm.writeSingleReg(rr, bits.RotateLeft8(vm.readSingleReg(rr), int(-x)))
 	}
 }
 
 func (vm *VM) rotlRegister(r, x uint8) {
+	rr := Register(r)
 	switch {
-	case isDoubleReg(r):
-		vm.writeDoubleReg(r, bits.RotateLeft16(vm.readDoubleReg(r), int(x)))
+	case IsDoubleReg(rr):
+		vm.writeDoubleReg(rr, bits.RotateLeft16(vm.readDoubleReg(rr), int(x)))
 	default:
-		vm.writeReg(r, bits.RotateLeft8(vm.readReg(r), int(x)))
+		vm.writeSingleReg(rr, bits.RotateLeft8(vm.readSingleReg(rr), int(x)))
 	}
 }
 
 func (vm *VM) jumpEq(r uint8, d uint16) {
-	if vm.readAnyReg(r) == uint16(vm.readReg(0)) {
+	if vm.ReadReg(Register(r)) == uint16(vm.readSingleReg(0)) {
 		vm.pc = d
 	}
 }
@@ -152,25 +162,27 @@ func (vm *VM) loadSPImm(d uint16) {
 }
 
 func (vm *VM) loadSPReg(d uint8) {
-	vm.sp = vm.readAnyReg(d)
+	vm.sp = vm.ReadReg(Register(d))
 }
 
 func (vm *VM) push(r uint8) {
+	rr := Register(r)
 	switch {
-	case isDoubleReg(r):
-		vm.push16(vm.readDoubleReg(r))
+	case IsDoubleReg(rr):
+		vm.push16(vm.readDoubleReg(rr))
 	default:
 		vm.sp--
-		vm.writeMem8(vm.sp, vm.readReg(r))
+		vm.writeMem8(vm.sp, vm.readSingleReg(rr))
 	}
 }
 
 func (vm *VM) pop(r uint8) {
+	rr := Register(r)
 	switch {
-	case isDoubleReg(r):
-		vm.writeDoubleReg(r, vm.pop16())
+	case IsDoubleReg(rr):
+		vm.writeDoubleReg(rr, vm.pop16())
 	default:
-		vm.writeReg(r, vm.readMem8(vm.sp))
+		vm.writeSingleReg(rr, vm.readMem8(vm.sp))
 		vm.sp++
 	}
 }
@@ -193,7 +205,7 @@ func (vm *VM) calla(pc uint16) {
 
 func (vm *VM) callr(r uint8) {
 	vm.push16(vm.pc)
-	vm.pc = vm.readAnyReg(r)
+	vm.pc = vm.ReadReg(Register(r))
 }
 
 func (vm *VM) rtn() {
--- pkg/vm/registers.go
+++ pkg/vm/registers.go
@@ -1,19 +1,39 @@
 package vm
 
-func isDoubleReg(r uint8) bool {
+type Register uint8
+
+// Register names
+const (
+	Register0 Register = iota
+	Register1
+	Register2
+	Register3
+	Register4
+	Register5
+	Register6
+	Register7
+	Register8
+	Register9
+	RegisterA
+	RegisterB
+	RegisterC
+	RegisterD
+)
+
+func IsDoubleReg(r Register) bool {
 	return r >= regA && r <= regD
 }
 
-func regWidth(r uint8) uint8 {
+func regWidth(r Register) uint8 {
 	if r <= 9 {
 		return 1
-	} else if isDoubleReg(r) {
+	} else if IsDoubleReg(r) {
 		return 2
 	}
 	return 0
 }
 
-func checkRegWidth(regs ...uint8) {
+func checkRegWidth(regs ...Register) {
 	w := regWidth(regs[0])
 	for _, r := range regs {
 		if regWidth(r) != w {
@@ -24,7 +44,7 @@ func checkRegWidth(regs ...uint8) {
 
 // width can be 1 or 2
 // return value will be 8 or 16-bit depending on width
-func (vm *VM) readMem(addr uint16, width int) uint16 {
+func (vm *VM) ReadMem(addr uint16, width int) uint16 {
 	switch width {
 	case 1:
 		return uint16(vm.memory[addr])
@@ -37,15 +57,15 @@ func (vm *VM) readMem(addr uint16, width int) uint16 {
 }
 
 func (vm *VM) readMem8(addr uint16) uint8 {
-	return uint8(vm.readMem(addr, 1))
+	return uint8(vm.ReadMem(addr, 1))
 }
 
 func (vm *VM) readMem16(addr uint16) uint16 {
-	return vm.readMem(addr, 2)
+	return vm.ReadMem(addr, 2)
 }
 
 // width can be 1 or 2
-func (vm *VM) writeMem(addr uint16, width int, val uint16) {
+func (vm *VM) WriteMem(addr uint16, width int, val uint16) {
 	switch width {
 	case 1:
 		vm.memory[addr] = uint8(val)
@@ -56,49 +76,49 @@ func (vm *VM) writeMem(addr uint16, width int, val uint16) {
 }
 
 func (vm *VM) writeMem8(addr uint16, val uint8) {
-	vm.writeMem(addr, 1, uint16(val))
+	vm.WriteMem(addr, 1, uint16(val))
 }
 
 func (vm *VM) writeMem16(addr uint16, val uint16) {
-	vm.writeMem(addr, 2, uint16(val))
+	vm.WriteMem(addr, 2, uint16(val))
 }
 
-func (vm *VM) writeAnyReg(r uint8, v uint16) {
+func (vm *VM) WriteReg(r Register, v uint16) {
 	switch {
-	case isDoubleReg(r):
+	case IsDoubleReg(r):
 		vm.writeDoubleReg(r, v)
 	default:
-		vm.writeReg(r, uint8(v))
+		vm.writeSingleReg(r, uint8(v))
 	}
 }
 
-func (vm *VM) readAnyReg(r uint8) uint16 {
+func (vm *VM) ReadReg(r Register) uint16 {
 	switch {
-	case isDoubleReg(r):
+	case IsDoubleReg(r):
 		return vm.readDoubleReg(r)
 	default:
-		return uint16(vm.readReg(r))
+		return uint16(vm.readSingleReg(r))
 	}
 }
 
-func (vm *VM) readAnyReg2Comp(r uint8) uint16 {
+func (vm *VM) readAnyReg2Comp(r Register) uint16 {
 	switch {
-	case isDoubleReg(r):
+	case IsDoubleReg(r):
 		return vm.readDoubleReg(r)
 	default:
-		return uint16(int8(vm.readReg(r)))
+		return uint16(int8(vm.readSingleReg(r)))
 	}
 }
 
-func (vm *VM) writeReg(r, v uint8) {
+func (vm *VM) writeSingleReg(r Register, v uint8) {
 	vm.registers[r] = v
 }
 
-func (vm *VM) readReg(r uint8) uint8 {
+func (vm *VM) readSingleReg(r Register) uint8 {
 	return vm.registers[r]
 }
 
-func (vm *VM) writeDoubleReg(r uint8, v uint16) {
+func (vm *VM) writeDoubleReg(r Register, v uint16) {
 	switch r {
 	case regA:
 		vm.registers[2] = uint8(v >> 8)
@@ -115,7 +135,7 @@ func (vm *VM) writeDoubleReg(r uint8, v uint16) {
 	}
 }
 
-func (vm *VM) readDoubleReg(r uint8) uint16 {
+func (vm *VM) readDoubleReg(r Register) uint16 {
 	switch r {
 	case regA:
 		return (uint16(vm.registers[2]) << 8) + uint16(vm.registers[3])
